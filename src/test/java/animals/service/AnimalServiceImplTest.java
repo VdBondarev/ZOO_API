086dd6a139f252794b81e9b3a2cfafb555fe3353
Vlad Bondarev <bondarev.21@icloud.com>
2024-04-26 09:56:13 +0300

    added test method for search() with valid params

--- src/test/java/animals/service/AnimalServiceImplTest.java
+++ src/test/java/animals/service/AnimalServiceImplTest.java
@@ -1,6 +1,10 @@
 package animals.service;
 
+import static animals.constants.CriteriaQueryConstantsHolder.COST_COLUMN;
+import static animals.constants.CriteriaQueryConstantsHolder.TYPE_COLUMN;
+import static animals.constants.CriteriaQueryConstantsHolder.WEIGHT_COLUMN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -17,9 +21,10 @@ import animals.service.parser.FileParserStrategy;
 import animals.service.parser.XmlFileParser;
 import animals.service.reader.CsvFileReader;
 import animals.service.reader.FileReaderStrategy;
-import java.util.List;
-
 import animals.service.reader.XmlFileReader;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,6 +32,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -147,7 +156,8 @@ class AnimalServiceImplTest {
             </animals>
                 """;
 
-        MultipartFile xmlFile = new MockMultipartFile("test.xml", "test.xml",
+        MultipartFile xmlFile;
+        xmlFile = new MockMultipartFile("test.xml", "test.xml",
                 "text/xml", xmlContent.getBytes());
 
         String[] buddyInfo = new String[] {"Buddy", "cat", "female", "51", "25"};
@@ -182,6 +192,85 @@ class AnimalServiceImplTest {
         assertEquals(2, actual.size());
     }
 
+    @Test
+    @DisplayName("Verify that getById works as expected with a valid id")
+    void getById_ValidId_ReturnsValidResponse() {
+        when(animalRepository.findById(1L)).thenReturn(Optional.of(buddyAnimal));
+        when(animalMapper.toResponseDto(buddyAnimal)).thenReturn(buddyResponseDto);
+
+        AnimalResponseDto expected = buddyResponseDto;
+        AnimalResponseDto actual = animalService.getById(1L);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Verify that getById works as expected with a non-valid id")
+    void getById_NonValidId_ThrowsException() {
+        when(animalRepository.findById(3L)).thenReturn(Optional.empty());
+
+        EntityNotFoundException exception =
+                assertThrows(EntityNotFoundException.class, () -> animalService.getById(3L));
+
+        String expected = "Can't find an animal by id 3";
+        String actual = exception.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Verify that search() works fine with valid params")
+    void search_ValidParams_ReturnsValidResponse() {
+        AnimalSearchParamsRequestDto paramsRequestDto =
+                createAnimalSearchParametersDto(
+                        null, "cat", null, List.of(55), List.of(50), null);
+
+        Specification<Animal> typeSpecification = (root, query, criteriaBuilder)
+                -> criteriaBuilder.like(root.get(TYPE_COLUMN), paramsRequestDto.type());
+
+        Specification<Animal> weightSpecification = (root, query, criteriaBuilder)
+                -> criteriaBuilder.between(
+                        root.get(WEIGHT_COLUMN), 0, paramsRequestDto.weight().get(0));
+
+        Specification<Animal> costSpecification = (root, query, criteriaBuilder)
+                -> criteriaBuilder.between(
+                        root.get(COST_COLUMN), 0, paramsRequestDto.cost().get(0));
+
+        Specification<Animal> specification;
+        specification = typeSpecification.and(weightSpecification).and(costSpecification);
+
+        PageRequest pageable = PageRequest.of(0, 5);
+        Page<Animal> page = new PageImpl<>(
+                List.of(buddyAnimal),
+                pageable,
+                List.of(buddyAnimal).size());
+
+        AnimalResponseDto expected = buddyResponseDto;
+
+        when(specificationBuilder.build(paramsRequestDto)).thenReturn(specification);
+        when(animalRepository.findAll(specification, pageable)).thenReturn(page);
+        when(animalMapper.toResponseDto(buddyAnimal)).thenReturn(expected);
+
+        List<AnimalResponseDto> expectedList = List.of(expected);
+        List<AnimalResponseDto> actualList = animalService.search(paramsRequestDto, pageable);
+
+        assertEquals(expectedList, actualList);
+        assertEquals(expected, actualList.get(0));
+    }
+
+    private AnimalSearchParamsRequestDto createAnimalSearchParametersDto(
+            String name, String type, String sex,
+            List<Integer> weight, List<Integer> cost, List<Long> categoriesIds) {
+        return new AnimalSearchParamsRequestDto(
+                name,
+                type,
+                sex,
+                weight,
+                cost,
+                categoriesIds
+        );
+    }
+
     private AnimalResponseDto createResponseDto(Animal animal) {
         return new AnimalResponseDto(
                 animal.getId(),
@@ -206,3 +295,19 @@ class AnimalServiceImplTest {
                 .setCost(cost);
     }
 }
+
+/**
+ *
+ *     @Override
+ *     public List<AnimalResponseDto> search(
+ *             AnimalSearchParamsRequestDto searchParams, Pageable pageable) {
+ *         if (isEmpty(searchParams)) {
+ *             throw new IllegalArgumentException("Searching should be done by at least 1 param.");
+ *         }
+ *         return animalRepository.findAll(
+ *                 specificationBuilder.build(searchParams), pageable)
+ *                 .stream()
+ *                 .map(animalMapper::toResponseDto)
+ *                 .collect(Collectors.toList());
+ *     }
+ */
