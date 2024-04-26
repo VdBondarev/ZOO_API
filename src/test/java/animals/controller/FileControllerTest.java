abd380b7c9bb525e211cae40f7d2cc371878f55b
Vlad Bondarev <bondarev.21@icloud.com>
2024-04-26 11:24:57 +0300

    fully implemented tests for /files/upload endpoint

--- src/test/java/animals/controller/FileControllerTest.java
+++ src/test/java/animals/controller/FileControllerTest.java
@@ -1,5 +1,9 @@
 package animals.controller;
 
+import static animals.holder.LinksHolder.CSV_FILE_PATH;
+import static animals.holder.LinksHolder.REMOVE_ALL_ANIMALS_FILE_PATH;
+import static animals.holder.LinksHolder.WORD_FILE_PATH;
+import static animals.holder.LinksHolder.XML_FILE_PATH;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -25,7 +30,6 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FileControllerTest {
     protected static MockMvc mockMvc;
-    private static final String VALID_CSV_FILE_PATH = "src/test/java/files/test.csv";
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -40,8 +44,12 @@ class FileControllerTest {
     @WithMockUser(username = "admin@example.com", authorities = "ADMIN")
     @Test
     @DisplayName("Verify that upload() method works as expected with .csv file")
+    @Sql(
+            scripts = REMOVE_ALL_ANIMALS_FILE_PATH,
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
     void upload_CsvFile_Success() throws Exception {
-        File file = new File(VALID_CSV_FILE_PATH);
+        File file = new File(CSV_FILE_PATH);
 
         byte[] fileContent = Files.readAllBytes(file.toPath());
 
@@ -59,7 +67,61 @@ class FileControllerTest {
         AnimalResponseDto[] animalResponseDtos = objectMapper.readValue(result.getResponse()
                 .getContentAsString(), AnimalResponseDto[].class);
 
+        int lengthExpected = 8;
+        Assertions.assertEquals(lengthExpected, animalResponseDtos.length);
+    }
+
+    @WithMockUser(username = "admin@example.com", authorities = "ADMIN")
+    @Test
+    @DisplayName("Verify that upload() method works as expected with .xml file")
+    @Sql(
+            scripts = REMOVE_ALL_ANIMALS_FILE_PATH,
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
+    void upload_XmlFile_Success() throws Exception {
+        File file = new File(XML_FILE_PATH);
+
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file", file.getName(), "text/xml", fileContent);
+
+        MvcResult result = mockMvc.perform(
+                        multipart("/files/upload")
+                                .file(multipartFile)
+                                .contentType(MediaType.MULTIPART_FORM_DATA)
+                )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        AnimalResponseDto[] animalResponseDtos = objectMapper.readValue(result.getResponse()
+                .getContentAsString(), AnimalResponseDto[].class);
+
         int lengthExpected = 7;
         Assertions.assertEquals(lengthExpected, animalResponseDtos.length);
+    }
+
+    @WithMockUser(username = "admin@example.com", authorities = "ADMIN")
+    @Test
+    @DisplayName("Verify that upload() method works as expected with .word file")
+    @Sql(
+            scripts = REMOVE_ALL_ANIMALS_FILE_PATH,
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
+    void upload_workFile_Fail() throws Exception {
+        File file = new File(WORD_FILE_PATH);
+
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file", file.getName(), "text/xml", fileContent);
+
+        MvcResult result = mockMvc.perform(
+                        multipart("/files/upload")
+                                .file(multipartFile)
+                                .contentType(MediaType.MULTIPART_FORM_DATA)
+                )
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 }
---
+++ src/test/java/animals/holder/LinksHolder.java
@@ -0,0 +1,9 @@
+package animals.holder;
+
+public interface LinksHolder {
+    String CSV_FILE_PATH = "src/test/java/files/test.csv";
+    String REMOVE_ALL_ANIMALS_FILE_PATH =
+            "classpath:database/remove-all-animals-from-animals-table.sql";
+    String XML_FILE_PATH = "src/test/java/files/test.xml";
+    String WORD_FILE_PATH = "src/test/java/files/nonvalid.word";
+}
